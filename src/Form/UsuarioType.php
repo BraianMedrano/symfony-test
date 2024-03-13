2e0684493c0dee0ec8014070b9962d24d14361a9
Braian Medrano <braiankpo_18@hotmail.com>
2024-03-13 15:36:05 -0300

    Se modifican elementos de la UI para mejor visualizacion

--- src/Form/UsuarioType.php
+++ src/Form/UsuarioType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Usuario;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,28 @@ class UsuarioType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre')
-            ->add('apellido')
-            ->add('edad')
+        // La estructura de los campos del formulario se pueden modificar, ya sea para agregarle un label, un placeholder, un atributo, etc.
+        // Por ejemplo en el formulario de abajo (lo de nombre no se puede modificar por que asi es la tabla en la base de datos) pero si los atributos, placeholder, etc.
+
+        // Tambien se puede cambiar el tipo de campo, por ejemplo si queremos que el campo de nombre sea un email, un password, etc. En el caso de abajo es un TextType
+            ->add('nombre', TextType::class, [
+                'label' => 'Nombre',
+                'attr' => [
+                    'placeholder' => 'Ingrese su nombre'
+                ]
+            ])
+            ->add('apellido', TextType::class, [
+                'label' => 'Apellido',
+                'attr' => [
+                    'placeholder' => 'Ingrese su apellido'
+                ]
+            ])
+            ->add('edad', IntegerType::class, [
+                'label' => 'Edad',
+                'attr' => [
+                    'placeholder' => 'Ingrese su edad'
+                ]
+            ])
         ;
     }
 
